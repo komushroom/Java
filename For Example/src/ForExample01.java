e468e22045697f61d1342971c7dc1307a15d35ac
komushroom <95523186+komushroom@users.noreply.github.com>
2022-05-11 16:37:37 +0900

    Add files via upload

---
+++ For Example/src/ForExample01.java
@@ -0,0 +1,12 @@
+
+public class ForExample01 {
+
+	public static void main(String[] args) {
+		// 반복문 - for문 1~5까지의 합 1+2+3+4+5
+		int sum = 0;
+		for(int i=1; i<=5; i++)
+			sum += i; // sum = sum+i
+		System.out.println(sum);
+	}
+
+}
---
+++ For Example/src/ForExample02.java
@@ -0,0 +1,11 @@
+
+public class ForExample02 {
+
+	public static void main(String[] args) {
+		// 반복문 for float 타입으로 실해
+		for(float x=0.1f; x<=1.0f; x+=0.1f) {
+		System.out.println(x);
+		}
+	}
+
+}
