06e56482548428bb27c3e213581193b5a4c44fa5
komushroom <95523186+komushroom@users.noreply.github.com>
2022-06-22 16:20:13 +0900

    Add files via upload

---
+++ ArrayExam/src/ArrayTest09.java
@@ -0,0 +1,12 @@
+
+public class ArrayTest09 {
+
+	public static void main(String[] args) {
+		// 2차원 배열
+		// int[][] score= new int [3][4];//0으로 초기화
+		int[][] score = {{75,98,88,58},{100,14,51,62},{82,28,47,57}};
+		System.out.println(score[2][3]);
+		System.out.println(score[1][0]);
+	}
+
+}
