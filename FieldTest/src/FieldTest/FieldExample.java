ab66ec961fffef5726541f42a227248a681b918e
komushroom <95523186+komushroom@users.noreply.github.com>
2022-06-29 15:38:25 +0900

    Add files via upload

---
+++ FieldTest/src/FieldTest/FieldExample.java
@@ -0,0 +1,20 @@
+package FieldTest;
+
+public class FieldExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//실행클래스 
+		FildeValue fiv = new FildeValue();
+		
+		System.out.println("byte필드 초기값:" + fiv.byteField);
+		System.out.println("char필드 초기값:" + fiv.charField);
+		System.out.println("short필드 초기값:" + fiv.shortField);
+		System.out.println("float필드 초기값:" + fiv.floatField);
+		System.out.println("String필드 초기값:" + fiv.StringField);
+		System.out.println("boolean필드 초기값:" + fiv.booleanField);
+		
+	}
+
+}
+ 
---
+++ FieldTest/src/FieldTest/FildeValue.java
@@ -0,0 +1,18 @@
+package FieldTest;
+
+public class FildeValue {
+	//라이브러리 클래스
+	byte byteField;
+	char charField;
+	short shortField;
+    float floatField = 3.14f;
+    String stringField;
+    boolean booleanField = true;
+    
+    //기본생성자- 생략가능
+    public FieldValue() {
+    	
+    }
+    
+    //매소드 
+}	
